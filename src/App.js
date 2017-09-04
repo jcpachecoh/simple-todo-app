8a51cb73b417e2336fca8bea4cec36a9c9c644d9
Juan Carlos Pacheco Hernandez <jcpachecoh@gmail.com>
2017-09-04 10:15:31 -0500

    simple crud todo

--- src/App.js
+++ src/App.js
@@ -3,17 +3,93 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  details(index) {
+    let todos = this.state.todos;
+
+    let todo = todos.find(function(todo) {
+      return todo.counter === index
+    });
+
+    console.log('you are clicking ');
+    console.log(todo);
+  }
+  removeTodo(index) {
+    let todos = this.state.todos;
+
+    let todo = todos.find(function(todo) {
+      return todo.counter === index;
+    });
+
+    todos.splice(todo,1);
+
+    this.setState({
+      todos: todos
+    })
+
+  }
+
+  addTodo(event) {
+    event.preventDefault();
+    let name= this.refs.name.value;
+    let completed= this.refs.completed.value;
+    let counter = this.state.counter;
+    
+
+    let todo = {
+      name,
+      completed,
+      counter
+    };
+    counter += 1;
+
+    let todos = this.state.todos;
+
+    todos.push(todo);
+
+    this.setState({
+      todos: todos,
+      counter: counter
+    })
+
+    this.refs.todoForm.reset();
+  }
+
+
+  constructor() {
+    super();
+    this.addTodo = this.addTodo.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
+    this.details = this.details.bind(this);
+    this.state = {
+      todos: [],
+      title: 'React simple todo application',
+      counter: 0
+    }
+  }
+
   render() {
+    let title = this.state.title;
+    let todos = this.state.todos;
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{title}</h1>
+        <form ref="todoForm">
+          <input type="text" ref="name" placeholder="write a task" />
+          <input type="text" ref="completed" />
+          <button onClick={this.addTodo}> Add Todo </button>
+        </form>
+        <pre>
+        {JSON.stringify(todos)}
+        </pre>
+        <ul>
+          {todos.map((todo => <li key={todo.counter}> {todo.name}
+            <button onClick={this.removeTodo.bind(null, todo.counter)}>Remove</button>
+            <button onClick={this.details.bind(null, todo.counter)}>View details</button>
+            </li>))}
+        </ul>
       </div>
+      
     );
   }
 }
